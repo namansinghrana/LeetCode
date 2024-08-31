73faa3743bfce178736bddb77e359bfbbbf39a68
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-08-31 07:15:43 +0530

    Time: 2167 ms (5.02%), Space: 56.7 MB (49.93%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -1,22 +1,22 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        double prob[] = new double[n];
-        prob[start_node]= 1;
-        
-        for(int i=0; i<n-1; i++){
-            int j=0;
-            for(int edge[] : edges){
-                int u= edge[0];
-                int v= edge[1];
-                double sp = succProb[j];
-                if(prob[u] * sp > prob[v]){
-                    prob[v] = prob[u] * sp;
-                }
-                if(prob[v] * sp > prob[u]){
-                    prob[u] = prob[v] * sp;
-                }
-                j++;
-            }
+       double prob[]= new double[n];
+       prob[start_node] = 1;
+
+       for(int i=0; i<n-1; i++){
+        int j=0;
+        for(int edge[] : edges){     
+        int u = edge[0];
+        int v = edge[1];
+        double w = succProb[j];
+        if(prob[u] * w > prob[v]){
+            prob[v] = prob[u] * w;
+        }
+        if(prob[v] * w > prob[u]){
+            prob[u] = prob[v] * w;
+        }
+        j++;
+        }
         }
         return prob[end_node];
     }
