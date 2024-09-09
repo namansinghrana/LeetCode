7cd9416c5d1e28cfe0bb2a59071808be929752b2
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-09-09 14:25:10 +0530

    Time: 49 ms (13.87%), Space: 44.3 MB (97.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,7 +4,7 @@ class Solution {
         int[] indices = new int[2];
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<=n-1;j++){
-                if(nums[i]+nums[j] == target){
+                if(target - nums[j] == nums[i]){
                     indices[0] = i;
                     indices[1] = j;
                 }
