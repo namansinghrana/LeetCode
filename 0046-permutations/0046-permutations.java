31dfa8c41caac3e564ec1f0f703470daf8b7a01a
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-09-10 22:54:38 +0530

    Time: 1 ms (94.41%), Space: 44.4 MB (52.04%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        perMute(nums ,0 ,result);
+        List<List<Integer>> result=  new ArrayList<>();
+        perMute(nums,0,result);
         return result;
     }
 
+    public void perMute(int[] nums,int currIndex,List<List<Integer>> result){
 
-    public void perMute(int[] nums ,int currIndex ,List<List<Integer>>result){
-        
         if(currIndex == nums.length-1){
-            List<Integer> currPer = new ArrayList<>();
-                for(int num : nums){
-                    currPer.add(num);
-                }
-            result.add(currPer);
+            List<Integer> currPermute = new ArrayList<>();
+            for(int num: nums){
+                currPermute.add(num);
+            }
+            result.add(currPermute);
             return;
         }
-
-        for(int index =currIndex; index < nums.length; index++){
+        for(int index=currIndex; index < nums.length ; index++){
             swap(nums,currIndex,index);
             perMute(nums,currIndex+1,result);
             swap(nums,currIndex,index);
         }
     }
 
-    public void swap(int[] nums,int i,int j){
+    public void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
