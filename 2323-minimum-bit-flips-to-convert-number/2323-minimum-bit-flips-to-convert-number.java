b09c50680f05030c630368e24661d4ea0d132e11
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-09-11 11:06:07 +0530

    Time: 0 ms (100%), Space: 40.2 MB (79.96%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int count=0;
+        int xor = start^goal;
+        while(xor != 0){
+            if((xor & 1) == 1) count++;
+            xor = xor >> 1;
+        }
+        return count;
+    }
+}
