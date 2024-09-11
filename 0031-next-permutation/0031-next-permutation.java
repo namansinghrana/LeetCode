313767196973b9db159280bb69588a1a2117c314
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-09-11 11:48:36 +0530

    Time: 0 ms (100%), Space: 42.9 MB (78.41%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,23 +1,23 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
+        int i= n-2;
 
-        int i = n-2;
-        while(i >= 0 && nums[i] >= nums[i+1]){
+        while(i >=0 && nums[i] >= nums[i+1]){
             i--;
         }
 
-        if(i >=0){
+        if(i >= 0){
             int j=n-1;
             while(nums[j] <= nums[i]){
                 j--;
             }
-
-            swap(nums, i, j);
+            swap(nums,i,j);
         }
 
         reverse(nums, i+1, n-1);
     }
+
 
     public void swap(int[] nums,int i,int j){
         int temp = nums[i];
@@ -25,12 +25,11 @@ class Solution {
         nums[j] = temp;
     }
 
-    public void reverse(int[] nums, int start, int end){
+    public void reverse(int[] nums,int start,int end){
         while(start < end){
             swap(nums, start, end);
             start++;
             end--;
         }
     }
-
 }
