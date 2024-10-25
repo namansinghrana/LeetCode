b5ac87d3621817c81548e7dc93de5a2e21e6e73b
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-10-26 00:04:23 +0530

    Time: 11 ms (34.78%), Space: 41.2 MB (36.12%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,42 +1,36 @@
 class Solution {
 
-    public boolean helper(char[][] board){
+    public static boolean helper(char[][] board){
+
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-
                 if(board[i][j] == '.'){
-                    for(char c='1'; c<= '9';c++){
-
+                    for(char c='1'; c<='9' ;c++){
                         if(isValid(board,i,j,c)){
                             board[i][j] = c;
-
-                            if(helper(board) == true){
-                                return true;
-                            }else{
-                                board[i][j] = '.';
-                            }
+                                if(helper(board)){
+                                    return true;
+                                }else{
+                                    board[i][j] = '.';
+                                }
                         }
                     }
-                return false;
-                }
+                    return false;
                 }
             }
+        }
         return true;
     }
 
-
-    
-    public static boolean isValid(char[][] board, int row, int col, char c){
-        for(int i=0;i<9;i++){
-            if(board[i][col] == c){
-                return false;
-            }
-
+    public static boolean isValid(char[][] board,int row,int col,int c){
+        for(int i=0; i<9 ;i++){
             if(board[row][i] == c){
                 return false;
             }
-
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == c){
+            if(board[i][col] == c){
+                return false;
+            }   
+            if( board[3*(row/3)+i/3][3*(col/3)+i%3] == c){
                 return false;
             }
         }
