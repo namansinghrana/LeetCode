360794c6fcef6bb457ca587329b0749da0ca6746
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-10-27 20:22:41 +0530

    Time: 2 ms (99.22%), Space: 48.1 MB (40.01%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,39 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int first=nums[0],second=nums[0];
+        int firstcounter=0,secondcounter=0;
 
-        Map<Integer,Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-
-        for(int num : nums){
-            map.put(num , map.getOrDefault(num , 0)+1);
-        }
-
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            if(entry.getValue() > nums.length/3){
-                list.add(entry.getKey());
+        for(int num:nums){
+            if(num == first){
+                firstcounter++;
+            }else if(num == second){
+                secondcounter++;
+            }else if(firstcounter == 0){
+                first=num;
+                firstcounter = 1;
+            }else if(secondcounter == 0){
+                second=num;
+                secondcounter = 1;
+            }else{
+                firstcounter--;
+                secondcounter--;
             }
         }
+
+        firstcounter=0;
+        secondcounter=0;
+
+        for(int num : nums){
+            if(num == first) firstcounter++;
+            if(num == second) secondcounter++;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        if(first == second){ list.add(first); }else{
+        if(firstcounter > (nums.length/3)) list.add(first);
+        if(secondcounter > (nums.length/3)) list.add(second);
+        }
+        
         return list;
     }
 }
