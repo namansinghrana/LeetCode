c82edbc1f1ba5771af10bb70348329993a057a21
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-11-02 10:23:45 +0530

    Time: 1 ms (96.81%), Space: 42.2 MB (12.59%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -3,8 +3,7 @@ class Solution {
         String[] str = sentence.split(" ");
         char temp = str[0].charAt(str[0].length()-1);
 
-        for(int i=1;i<str.length; i++){
-            
+        for(int i=1; i<str.length; i++){
             if(temp != str[i].charAt(0)){
                 return false;
             }
