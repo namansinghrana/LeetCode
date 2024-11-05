0a75ee9fe49948fd1cab0d54fb46adec0e20ffef
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-11-05 04:49:57 -0800

    Time: 3 ms (97.78%), Space: 44.5 MB (85.94%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -1,25 +1,10 @@
 class Solution {
     public int minChanges(String s) {
-        int n = s.length();
-        char ch =s.charAt(0);
-        int count=0;
         int changes=0;
-
-        for(int i=0; i<n; i++){
-
-            if(s.charAt(i) == ch){
-                count++;
-                continue;
-            }
-
-            //if code comes here it means its not equal to the previous character so...
-            if(count%2 == 0){
-                count = 1;
-            }else{
+        for(int i=0;i<s.length();i+=2){
+            if(s.charAt(i) != s.charAt(i+1)){
                 changes++;
-                count = 0;
-            }            
-            ch = s.charAt(i);
+            }
         }
         return changes;
     }
