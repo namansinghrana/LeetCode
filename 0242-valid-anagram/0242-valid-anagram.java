70146512871ffdb547534beabf6701b933b7ab7d
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-12-23 09:28:06 +0530

    Time: 4 ms (76.69%), Space: 43.6 MB (63.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,14 +4,14 @@ class Solution {
 
         for(char c : s.toCharArray()){
             count[c-'a']++;
-        }   
+        }
 
         for(char c : t.toCharArray()){
             count[c-'a']--;
         }
 
-        boolean all_Zeroes = Arrays.stream(count).allMatch(element -> element == 0);
 
-        return all_Zeroes;
+        boolean allZeroes = Arrays.stream(count).allMatch(element -> element == 0);
+        return allZeroes;
     }
 }
