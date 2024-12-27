f4f1e069620971014f327e6600ccbbe4fa53ba32
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-12-27 12:26:40 +0530

    Time: 13 ms (75.28%), Space: 45.6 MB (86.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -3,15 +3,17 @@ class Solution {
         Map<Integer,Integer> map = new HashMap<>();
         for(int num : nums){ map.put(num, map.getOrDefault(num, 0)+1); }
 
-        List<int[]> arr=  new ArrayList<>();
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b)->a[0]-b[0]);
         for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            arr.add(new int[] {entry.getValue(), entry.getKey()});
-        }   
-        arr.sort((a, b)->b[0]-a[0]);
+            heap.offer(new int[] {entry.getValue(), entry.getKey()});
+            if(heap.size() > k){
+                heap.poll();
+            }
+        } 
 
         int[] res = new int[k];
         for(int i=0;i<k;i++){
-            res[i] = arr.get(i)[1];
+            res[i] = heap.poll()[1];
         }
         return res;
     }
