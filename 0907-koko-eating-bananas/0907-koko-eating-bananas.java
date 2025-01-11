1ced6ae01741544952d3f9c04e5d944bed79ffb9
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-01-11 20:40:03 +0530

    Time: 29 ms (10.62%), Space: 45.3 MB (31.33%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -4,19 +4,14 @@ class Solution {
         int r =Arrays.stream(piles).max().getAsInt();
         int res = r;
         while(l <= r){
-            int k = (l+r)/2;
-            long totalTime=0;
-            for(int p : piles){
-                totalTime += Math.ceil((double)p/k);
-            }
+            int k  = (l+r)/2;
 
+            long totalTime = 0;
+            for(int p : piles){ totalTime += Math.ceil((double)p/k); }
             if(totalTime <= h){
                 res = k;
                 r = k-1;
-            }else{
-                l = k+1;
-            }
-
+            }else{ l = k+1; }
         }
         return res;
     }
