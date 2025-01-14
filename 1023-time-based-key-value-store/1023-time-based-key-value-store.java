2bee2dde32bda7abf4c338785a11617ea7f01760
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-01-14 10:44:24 +0530

    Time: 146 ms (53.69%), Space: 119.6 MB (61.71%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.java
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.java
@@ -1,20 +1,47 @@
 class TimeMap {
-    private Map<String, TreeMap<Integer, String>> m;
+    private Map<String, List<Pair<Integer, String>>> keyStore;
     public TimeMap() {
-        m = new HashMap<>();
+        keyStore = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        m.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
+        keyStore.computeIfAbsent(key, k -> new ArrayList<>()).add(new Pair<>(timestamp, value));        
     }
     
     public String get(String key, int timestamp) {
-        if(!m.containsKey(key)){ return ""; }
+        List<Pair<Integer, String>> values = keyStore.getOrDefault(key, new ArrayList<>());
+        int left=0, right=values.size()-1;
+        String result= "";
 
-        TreeMap<Integer, String> currMap =  m.get(key);
-        Map.Entry<Integer, String> entry = currMap.floorEntry(timestamp);
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(values.get(mid).getKey() <= timestamp){
+                result = values.get(mid).getValue();
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return result;
+    }
 
-        return entry == null ? "" : entry.getValue();
+
+    private static class Pair<K, V>{
+        private final K key;
+        private final V value;
+
+        public Pair(K key,V value){
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey(){
+            return key;
+        }
+
+        public V getValue(){
+            return value;
+        }
     }
 }
 
