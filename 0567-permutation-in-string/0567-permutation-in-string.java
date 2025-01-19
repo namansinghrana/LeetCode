9080e009f1cf615b57342bcaad6b45e29f28044b
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-01-19 12:13:00 +0530

    Time: 5 ms (96.19%), Space: 42.7 MB (76.47%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,13 +2,19 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        char[] sortedS1 = s1.toCharArray();
-        Arrays.sort(sortedS1);
+        if(n > m){ return false; }
         
-        for(int i=0; i<=m-n; i++){
-            char[] sortedTemp = s2.substring(i, i+n).toCharArray();
-            Arrays.sort(sortedTemp);
-            if(Arrays.equals(sortedTemp, sortedS1)){ return true; }
+        int[] s1_freq =new int[26];
+        int[] s2_freq =new int[26];
+        for(int i=0;i<n;i++){ s1_freq[s1.charAt(i)-'a']++; }
+
+        int i=0;
+        int j=0;
+        while(j < m){   
+            s2_freq[s2.charAt(j)-'a']++;
+            if(j-i+1 > n){ s2_freq[s2.charAt(i)-'a']--; i++; }
+            if(Arrays.equals(s1_freq, s2_freq)){ return true; }
+            j++;
         }
         return false;
     }
