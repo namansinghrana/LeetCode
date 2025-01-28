938de610757cc5834296203341a6f084ff39eb3e
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-01-28 09:17:37 +0530

    Time: 47 ms (56.51%), Space: 116.4 MB (32.74%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,42 +1,70 @@
-class LRUCache {
-    private ArrayList<int[]> cache;
-    private int capacity;
-    public LRUCache(int capacity) {
-        this.cache = new ArrayList<>();
-        this.capacity = capacity;
-    }
-    
-    public int get(int key) {
-        for(int i=0;i<cache.size();i++){
-            if(cache.get(i)[0] == key){
-                int[] tmp = cache.remove(i);
-                cache.add(tmp);
-                return tmp[1];
-            }
-        }
-        return -1;
-    }
-    
-    public void put(int key, int value) {
-        for(int i=0; i<cache.size();i++){
-            if(cache.get(i)[0] == key){
-                int[] tmp = cache.remove(i);
-                tmp[1] = value;
-                cache.add(tmp);
-                return ;
-            }
-        }
+public class Node {
+    int key;
+    int val;
+    Node prev;
+    Node next;
 
-        if(capacity == cache.size()){
-            cache.remove(0);
-        }
-        cache.add(new int[]{key,value});
+    public Node(int key, int val) {
+        this.key = key;
+        this.val = val;
+        this.prev = null;
+        this.next = null;
     }
 }
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
+ class LRUCache {
+    
+    private int cap;
+    private HashMap<Integer, Node> cache;
+    private Node left;
+    private Node right;
+
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+        this.cache = new HashMap<>();
+        this.left = new Node(0, 0);
+        this.right = new Node(0, 0);
+        this.left.next = this.right;
+        this.right.prev = this.left;
+    }
+
+    private void remove(Node node) {
+        Node prev = node.prev;
+        Node nxt = node.next;
+        prev.next = nxt;
+        nxt.prev = prev;
+    }
+
+    private void insert(Node node) {
+        Node prev = this.right.prev;
+        prev.next = node;
+        node.prev = prev;
+        node.next = this.right;
+        this.right.prev = node;
+    }
+
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            remove(node);
+            insert(node);
+            return node.val;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+        }
+        Node newNode = new Node(key, value);
+        cache.put(key, newNode);
+        insert(newNode);
+
+        if (cache.size() > cap) {
+            Node lru = this.left.next;
+            remove(lru);
+            cache.remove(lru.key);
+        }
+    }
+}
