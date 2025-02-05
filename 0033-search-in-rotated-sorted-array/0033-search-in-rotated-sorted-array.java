21772628f67916c5c46966604b76359efffc91d1
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-02-05 09:03:03 +0530

    Time: 0 ms (100%), Space: 41.8 MB (91.77%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,25 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l=0;
-        int r=nums.length-1;
-        while(l <= r){
-            int mid = (l+r)/2;
-            if(target == nums[mid]) { return mid; }
+        int start=0;
+        int end=nums.length-1;
+        return binarySearchSorted(nums, target, start, end);
+    }
 
-            if(nums[l] <= nums[mid]){
-                if(target < nums[l] || target > nums[mid]){
-                    l = mid + 1;
-                }else{
-                    r = mid - 1;
-                }
+    static int binarySearchSorted(int[] nums, int target, int start, int end){
+        if(start > end){ return -1; }
+        int mid = start + (end-start)/2;
+        if (nums[mid] == target){ return mid; }
+
+        if(nums[start] <= nums[mid]){
+            if(nums[start] <= target && target <= nums[mid]){
+                return binarySearchSorted(nums, target, start, mid-1);
             }else{
-                if(target < nums[mid] || target > nums[r]){
-                    r = mid - 1;
-                }else{
-                    l = mid + 1;
-                }
+                return binarySearchSorted(nums, target, mid+1, end);
             }
         }
-        return -1;
+
+        if(target >= nums[mid] && target <= nums[end]){
+            return binarySearchSorted(nums, target, mid+1, end);
+        }
+        
+        return binarySearchSorted(nums, target, start, mid-1);
     }
 }
