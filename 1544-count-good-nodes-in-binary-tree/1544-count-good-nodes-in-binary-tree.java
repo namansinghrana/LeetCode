066dbe2e344029abe06e4d350d429dbf7548f616
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-02-11 05:22:11 +0530

    Time: 2 ms (85.53%), Space: 52.3 MB (75.23%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
@@ -15,16 +15,16 @@
  */
 class Solution {
     public int goodNodes(TreeNode root) {
-        return dfs(root,root.val);
+        return dfs(root, root.val);
     }
 
-    private static int dfs(TreeNode root,int max){
-        if(root == null){ return 0; }
-        
-        int res=(root.val >= max) ? 1 : 0 ;
-        max = Math.max(max, root.val);
-        res += dfs(root.left, max);
-        res += dfs(root.right, max);
+    private static int dfs(TreeNode node, int max){
+        if(node == null){ return 0; }
+
+        int res = (node.val >= max) ? 1 : 0;
+        max =  Math.max(max, node.val);
+        res += dfs(node.left, max);
+        res += dfs(node.right, max);
         return res;
     }
 }
