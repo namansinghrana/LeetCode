ea576417ce2db666bbe77735d3878628a7423fd2
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-02-15 05:35:44 +0530

    Time: 222 ms (3.34%), Space: 44.6 MB (36.68%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -20,19 +20,19 @@ class Solution {
         return res;
     }
 
-    private int getMax(TreeNode root){
+    private static int getMax(TreeNode root){
         if(root == null) return 0;
         int left = getMax(root.left);
         int right = getMax(root.right);
         int path = root.val + Math.max(left, right);
-        return Math.max(path, 0);
+        return Math.max(0, path);
     }
 
     private void dfs(TreeNode root){
         if(root == null) return;
         int left = getMax(root.left);
         int right = getMax(root.right);
-        res = Math.max(res, root.val + left + right);
+        res = Math.max(res, root.val + left + right );
         dfs(root.left);
         dfs(root.right);
     }
