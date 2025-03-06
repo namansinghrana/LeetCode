109e329609dec8e1ef8e21b9815620d024cc17f6
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-03-06 10:12:56 +0530

    Time: 1160 ms (8.99%), Space: 58.6 MB (6.95%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,37 +1,28 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length-1);
+        quickSort(nums, 0, nums.length-1);
         return nums;
     }
-
-    private static void mergeSort(int[] arr, int left, int right){
-        if(left < right){
-            int mid = left + (right - left)/2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid+1, right);
-            merge(arr, left, mid, right);
+    private static void quickSort(int[] arr, int low, int high){
+        if(low < high){
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot);
+            quickSort(arr, pivot+1, high);
         }
     }
-    
-    private static void merge(int[] arr, int left, int mid, int right){
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-        int[] L = new int[n1];
-        int[] R = new int[n2];
 
-        for(int i=0;i<n1;i++){ L[i] = arr[left + i]; }
-        for(int i=0;i<n2;i++){ R[i] = arr[mid + 1 + i]; }
+    private static int partition(int[] arr, int low, int high){
+        int pivot = arr[low];
+        int i=low-1, j=high+1;
 
-        int i=0, j=0, k=left;
-        while(i < n1 && j < n2){
-            if(L[i] <= R[j]){
-                arr[k++] = L[i++];
-            }else{
-                arr[k++] = R[j++];
-            }
+        while(true){
+            do{ i++; }while(arr[i] < pivot);
+            do{ j--; }while(arr[j] > pivot);
+            if(i >= j) return j;
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
-
-        while(i < n1){ arr[k++] = L[i++]; }
-        while(j < n2){ arr[k++] = R[j++]; }
     }
 }
