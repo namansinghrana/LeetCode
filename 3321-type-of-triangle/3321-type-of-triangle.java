54488881fff1e1679ea264c4f0e31f37452b65c1
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-05-19 06:21:26 +0530

    Time: 1 ms (34.56%), Space: 42.1 MB (43.93%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        Arrays.sort(nums);
+        int a = nums[0], b = nums[1], c = nums[2];
+        if (a + b <= c) return "none";
+        if (a == b && b == c) return "equilateral";
+        if (a == b || b == c || a == c) return "isosceles";
+        return "scalene";
+    }
+}
