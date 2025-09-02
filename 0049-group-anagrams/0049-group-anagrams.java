d475c82bca27d2e0862574ea61f4dfcf2065f63d
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-09-02 12:38:07 +0530

    Time: 10 ms (32.06%), Space: 49.4 MB (7.22%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,33 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> result = new HashMap<>();
 
-        for(String s : strs){
-            int[] count = new int[26];
-            for(char c : s.toCharArray()){
-                count[c-'a']++;
+        String[] substitute = Arrays.stream(strs)
+                .map(s -> {
+                    char[] chars = s.toCharArray();
+                    Arrays.sort(chars);
+                    return new String(chars);
+                })
+                .toArray(String[]::new);
+
+        Map<String, List<Integer>> map = new HashMap<>();
+        for(int i=0;i<substitute.length;i++){
+            if(map.containsKey(substitute[i])){
+                map.get(substitute[i]).add(i);
+            }else{
+                map.put(substitute[i], new ArrayList<>(List.of(i)));
             }
-            String key = Arrays.toString(count);
-
-            result.putIfAbsent(key, new ArrayList<>());
-            result.get(key).add(s);
         }
-        return new ArrayList<>(result.values());
+
+        
+        List<List<String>> groups = new ArrayList<>(map.size());
+        
+        for(List<Integer> indexes : map.values()){
+            List<String> group = new ArrayList<>();
+            for(int idx: indexes){
+                group.add(strs[idx]);
+            }
+            groups.add(group);
+        }
+        return groups;
     }
 }
