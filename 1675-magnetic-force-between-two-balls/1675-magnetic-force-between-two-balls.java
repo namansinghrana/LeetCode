c4c734c2a32f73303c9934934ac61a5ec33f2d6f
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-10-12 14:48:44 +0530

    Time: 42 ms (71.84%), Space: 55.8 MB (76.42%) - LeetHub

--- 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.java
@@ -1,9 +1,13 @@
 class Solution {
     public int maxDistance(int[] position, int m) {
+        //Sorted position to create my range
         Arrays.sort(position);
-        int left=0;
-        int right= position[position.length-1] - position[0];
+
+        int left=0, right= position[position.length-1] - position[0];
+        //Answer variable - 
+        // Required Force / Number of Max windows 
         int result= 0;
+        //Binary Search Over Range - Positions.
         while(left <= right){
             int mid = left + (right-left)/2;
             if(canPlaceBalls(position, m, mid)){
@@ -16,6 +20,8 @@ class Solution {
         return result;
     }
 
+    //function for counting the number of windows we found
+    //while simultaneously increasing the distance every iteration
     private static boolean canPlaceBalls(int[] position, int m, int distance){
         int count=1;
         int lastPlaced=position[0];
