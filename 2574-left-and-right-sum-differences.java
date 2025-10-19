83dc3b4f231c550028460921438c1044496104f4
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-10-19 10:05:27 +0530

    Create 2574-left-and-right-sum-differences.java

---
+++ 2574-left-and-right-sum-differences.java
@@ -0,0 +1,17 @@
+//Solved using prefix Sum and the differences between the prefix sums.
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int n = nums.length;
+        int[] leftSum= new int[n];
+        int[] rightSum= new int [n];
+        leftSum[0]=0; rightSum[n-1]=0;
+        for(int i=0;i<n-1;i++){ leftSum[i+1] = leftSum[i] + nums[i]; }
+        for(int i=n-1;i>0;i--){ rightSum[i-1] = rightSum[i] + nums[i]; }
+        
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            ans[i] = Math.abs(leftSum[i] - rightSum[i]);
+        }
+        return ans;
+    }
+}
