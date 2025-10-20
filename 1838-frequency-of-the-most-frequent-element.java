12dcfe92de8eff6af36fbca9468215a179d232c6
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-10-20 11:23:15 +0530

    Create 1838-frequency-of-the-most-frequent-element.java

---
+++ 1838-frequency-of-the-most-frequent-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left=0,right=0;
+        long res=0, total=0;
+        while(right < nums.length){
+            total += nums[right];
+            while(nums[right] * (right-left+1L) > total+k ){
+                total -= nums[left];
+                left++;
+            }
+            res = Math.max(res, right-left+1L);
+            right++;
+        }
+
+        return (int)res;
+    }
+}
