4d97d003138c3e05bd059ec6097f8202601b9f3e
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-10-22 12:29:36 +0530

    Update 0033-search-in-rotated-sorted-array.java

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -25,3 +25,30 @@ class Solution {
         return binarySearchSorted(nums, target, start, mid-1);
     }
 }
+
+//New Solution
+//Simpler One
+class Solution {
+    public int search(int[] nums, int target) {
+        int left=0, right= nums.length-1;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(nums[mid] == target) return mid;
+
+            if(nums[left] <= nums[mid]){
+                if(target >= nums[left] && target <= nums[mid]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }else{
+                if(target >= nums[mid] && target <= nums[right]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
