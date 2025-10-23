f01455c6703adb4264e77dc0576c69392df909fc
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-10-23 15:43:05 +0530

    Update 0069-sqrtx.java

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -19,3 +19,25 @@ class Solution {
         return Math.round(end);
     }
 }
+
+//New one
+class Solution {
+    public int mySqrt(int x) {
+        int left=0, right = x/2;
+
+        if(x < 2) return x;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            long square = (long)mid * mid;
+            if(square == x){
+                return mid;
+            }else if(square < x){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return left-1;
+    }
+}
