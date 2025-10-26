f7593952d8196c8bc6fecfa6a94b07fcd9b1c51d
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-10-26 12:35:09 +0530

    Update 0074-search-a-2d-matrix.java

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -16,3 +16,27 @@ class Solution {
         return false;
     }
 }
+
+//Another one
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for(int i=0;i<n;i++){
+            if(matrix[i][0] <= target){
+                int left=0, right = m-1;
+                while(left <= right){
+                    int mid = left + (right-left)/2;
+                    if(matrix[i][mid] == target){
+                        return true;
+                    }else if(matrix[i][mid] > target){
+                        right = mid-1;
+                    }else{
+                        left = mid+1;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
