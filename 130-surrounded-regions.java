9435497d693a7e80f5b01944232577ea88202abb
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-10-26 11:36:55 +0530

    Update 130-surrounded-regions.java

--- 130-surrounded-regions.java
+++ 130-surrounded-regions.java
@@ -1,40 +1,41 @@
 class Solution {
     public void solve(char[][] board) {
-        int rows=board.length;
-        int cols = board[0].length;
+        int n = board.length;
+        int m = board[0].length;
         Queue<int[]> queue = new LinkedList<>();
         int[][] dir = {{-1,0},{1,0},{0,1},{0,-1}};
 
-        for(int i=0;i<rows;i++){
-            if(board[i][0] == 'O') queue.add(new int[]{i, 0});
-            if(board[i][cols-1] == 'O') queue.add(new int[]{i, cols-1});
+        for(int i=0;i<n;i++){
+            if(board[i][0] == 'O') queue.add(new int[]{i,0});
+            if(board[i][m-1] == 'O') queue.add(new int[]{i, m-1});
         }
 
-        for(int j=0;j<cols;j++){
-            if(board[0][j] == 'O') queue.add(new int[]{0, j});
-            if(board[rows-1][j] == 'O') queue.add(new int[]{rows-1, j});
+        for(int j=0;j<m;j++){
+            if(board[0][j] == 'O') queue.add(new int[] {0, j});
+            if(board[n-1][j] == 'O') queue.add(new int[] {n-1, j});
         }
 
         while(!queue.isEmpty()){
             int[] cell = queue.poll();
-            int x = cell[0], y = cell[1];
+            int x=cell[0], y=cell[1];
 
-            if(board[x][y] != 'O') continue;
-
+            if(board[x][y] == 'X') continue;
+            
             board[x][y] = 'S';
 
             for(int[] d : dir){
                 int newX = x + d[0], newY = y + d[1];
-
-                if(newX >= 0 && newX < rows
-                && newY >= 0 && newY < cols
-                && board[newX][newY] == 'O')
-                queue.add(new int[]{newX, newY});
+                if(newX >= 0 && newX < n 
+                && newY >= 0 && newY < m
+                && board[newX][newY] == 'O'){
+                    queue.add(new int[] {newX, newY});
+                }
             }
         }
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(board[i][j] == 'O') board[i][j] = 'X';
                 else if(board[i][j] == 'S') board[i][j] = 'O';
             }
