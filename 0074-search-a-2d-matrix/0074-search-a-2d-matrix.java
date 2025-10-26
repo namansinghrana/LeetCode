f0a8a769baa8259b0bfc64ae669676abc633862f
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-10-26 12:38:31 +0530

    Update 0074-search-a-2d-matrix.java

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -23,7 +23,7 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         for(int i=0;i<n;i++){
-            if(matrix[i][0] <= target){
+            if(matrix[i][0] <= target && target <= matrix[i][m-1]){
                 int left=0, right = m-1;
                 while(left <= right){
                     int mid = left + (right-left)/2;
