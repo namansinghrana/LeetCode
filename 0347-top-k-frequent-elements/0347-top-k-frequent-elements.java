5ab86a61fc2b346eb45dd95c041639cc91fd6078
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-11-04 16:04:36 +0530

    Update 0347-top-k-frequent-elements.java

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,3 +1,4 @@
+//Complexity O(n log k)
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer,Integer> map = new HashMap<>();
@@ -15,6 +16,31 @@ class Solution {
         for(int i=0;i<k;i++){
             res[i] = heap.poll()[1];
         }
+        return res;
+    }
+}
+//Complexity O(n)
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer, Integer> freq = new HashMap<>();
+        for(int num : nums){ freq.put(num, freq.getOrDefault(num, 0)+1); }
+
+        List<Integer>[] bucket = new List[n+1];
+
+        for(int key : freq.keySet()){
+            int counting = freq.get(key);
+            if(bucket[counting] == null){ bucket[counting] = new ArrayList<>(); }
+            bucket[counting].add(key);
+        }
+
+        List<Integer> resList = new ArrayList<>();
+        for(int i = bucket.length-1; i>=0 && resList.size() < k; i--){
+            if(bucket[i] != null){ resList.addAll(bucket[i]); }
+        }
+
+        int[] res = new int[k];
+        for(int i=0;i<k;i++){res[i] = resList.get(i);}
         return res;
     }
 }
