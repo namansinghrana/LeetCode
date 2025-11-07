fa747253c6c0a8166f28a774352f49010a6b6e74
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-11-07 10:06:10 +0530

    Update 0340-longest-substring-with-at-most-k-distinct-characters.java

--- 0340-longest-substring-with-at-most-k-distinct-characters.java
+++ 0340-longest-substring-with-at-most-k-distinct-characters.java
@@ -1,19 +1,28 @@
 //Most optimal using the HashMap Technique
 class Solution {
-    public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        // Your code here
+    public int longestKSubstr(String s, int k) {
+        // code here
+        if(s == null || s.length() == 0 || k == 0 | k > s.length()) return 0;
+        
+        
+        Set<Character> checkUnique = new HashSet<>();
+        for(char c : s.toCharArray()) checkUnique.add(c);
+        
+        if(checkUnique.size() < k) return -1;
+        
         Map<Character, Integer> map = new HashMap<>();
         int start=0, maxLen=0;
         for(int end=0; end < s.length(); end++){
             map.put(s.charAt(end), map.getOrDefault(s.charAt(end), 0)+1);
         
-            if(map.size() > k){
+            while(map.size() > k){
                 map.put(s.charAt(start), map.get(s.charAt(start))-1);
                 if(map.get(s.charAt(start)) == 0) map.remove(s.charAt(start));
                 start++;
             }
-            if(map.size() == k) maxLen = Math.max(maxLen, end-start+1);
+            maxLen = Math.max(maxLen, end-start+1);
         }
         return maxLen;
+    
     }
 }
