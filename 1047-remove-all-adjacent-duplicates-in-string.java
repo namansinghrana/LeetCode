882e0632c49dd0f749fb5c5cccf0780935725cdd
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-11-11 10:58:19 +0530

    Update 1047-remove-all-adjacent-duplicates-in-string.java

--- 1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string.java
@@ -17,3 +17,19 @@ class Solution {
         return sb.reverse().toString();
     }
 }
+
+//Now this is the optimized approach
+//Remember the extra data structures may not be a problem here but make the program slower when huge inputs will be inserted. 
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){ 
+            int len = sb.length();
+            if(len > 0 && sb.charAt(len-1) == c){
+                sb.deleteCharAt(len-1);
+            }else{ sb.append(c); }
+        }
+        
+        return sb.toString();
+    }
+}
