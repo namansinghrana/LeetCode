887c9ccb10ec0c79900bb267c10909892e8894c9
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-12-06 12:12:22 +0530

    Refactor maxPathSum method for clarity and efficiency
    Refactor maxPathSum to use height method for calculating maximum path sum and handle negative values.

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -1,3 +1,5 @@
+// New code with understading of calculating the maximum path along with calculating height and handling negative values.
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -14,26 +16,23 @@
  * }
  */
 class Solution {
-    int res = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        dfs(root);
-        return res;
+        int[] max = new int[1];
+        max[0] = Integer.MIN_VALUE;
+        height(root, max);
+        return max[0];
     }
 
-    private static int getMax(TreeNode root){
+    private int height(TreeNode root,int[] max){
         if(root == null) return 0;
-        int left = getMax(root.left);
-        int right = getMax(root.right);
-        int path = root.val + Math.max(left, right);
-        return Math.max(0, path);
-    }
+        
+        //go left and then right
+        int left = Math.max(0, height(root.left, max));
+        int right = Math.max(0, height(root.right, max));
 
-    private void dfs(TreeNode root){
-        if(root == null) return;
-        int left = getMax(root.left);
-        int right = getMax(root.right);
-        res = Math.max(res, root.val + left + right );
-        dfs(root.left);
-        dfs(root.right);
+        // now at every moment check if you found out the max path.
+        max[0] = Math.max(max[0], left + right + root.val);
+
+        return root.val + Math.max(left, right);
     }
 }
