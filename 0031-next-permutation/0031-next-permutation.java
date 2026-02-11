1820e9a377f6264bbefc2fa532a2bb5e981b446e
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2026-02-11 20:04:52 +0530

    Implement next permutation algorithm

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -33,3 +33,49 @@ class Solution {
         }
     }
 }
+
+
+//Latest 2026 Code written by me without any help.
+class Solution {
+    public void nextPermutation(int[] nums) {
+
+        int n = nums.length;
+        //finding the breakpoint
+        int idx = -1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                idx = i;
+                break;
+            }
+        }
+
+        //if index not changes
+        if(idx != -1){
+            //fixing the ending rythm
+            for(int i=n-1;i>=0;i--){
+                if(nums[i] > nums[idx]){
+                    int temp = nums[i];
+                    nums[i] = nums[idx];
+                    nums[idx] = temp;
+                    break;
+                }
+            }
+
+            reverse(nums, idx+1, n-1);
+        }else{
+            //if index changes
+            reverse(nums, 0, n-1);
+
+        }
+    }
+
+    private void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
