f6d44c62dc39ee8fc66ede4074088340e61a2875
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2026-02-12 11:22:16 +0530

    Implement brute force and revised Kadane's algorithm
    Added a brute force solution and a revised Kadane's algorithm solution for finding the maximum subarray sum.

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -15,3 +15,39 @@ class Solution {
         return max;
     }
 }
+
+
+//Revision Brute Force Solution 2026
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int n = nums.length;
+        int res=0;
+        for(int i=0;i<n;i++){
+            
+            for(int j=i+1;j<n;j++){
+                currSum += nums[j];
+                res = Math.max(res, currSum);
+            }
+        }
+        return res;
+    }
+}
+
+//Revision Revised Kadane's Algo Solution 2026
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int n = nums.length;
+        int currSum =0, res=0;
+        for(int i=0;i<n;i++){
+            if(currSum + nums[i] < nums[i]){
+                currSum = nums[i];
+            }else{  
+                currSum += nums[i]; 
+            }
+            res = Math.max(res, currSum);
+        }
+    return res;
+    }
+}
